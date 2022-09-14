21be239b3360e76eee44c2708c70d6c1629ea3db
bezbiletniy <a.borodin.ca@gmail.com>
2022-09-14 22:16:56 +0300

    Add test 4

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -10,6 +10,7 @@ import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
@@ -23,7 +24,7 @@ class DeliveryTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() {
+    void shouldSuccessfulPlanAndReplanMeeting() throws InterruptedException {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
@@ -34,13 +35,15 @@ class DeliveryTest {
         $x("//*[@data-test-id=\"name\"]//self::input").setValue(validUser.getName());
         $x("//*[@data-test-id=\"phone\"]//self::input").setValue(validUser.getPhone());
         $x("//*[@data-test-id=\"agreement\"]").click();
-        $x("//*[@class=\"button__text\"]").click();
-        $x("//*[@data-test-id='notification']").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + firstMeetingDate), Duration.ofSeconds(15));
-
-        // TODO: добавить логику теста в рамках которого будет выполнено планирование и перепланирование встречи.
-        // Для заполнения полей формы можно использовать пользователя validUser и строки с датами в переменных
-        // firstMeetingDate и secondMeetingDate. Можно также вызывать методы generateCity(locale),
-        // generateName(locale), generatePhone(locale) для генерации и получения в тесте соответственно города,
-        // имени и номера телефона без создания пользователя в методе generateUser(String locale) в датагенераторе
+        $x("//span[text()='Запланировать']").click();
+        $x("//*[@data-test-id=\"success-notification\"]").shouldBe(visible).shouldHave(text("Успешно!\n" + "Встреча успешно запланирована на " + firstMeetingDate));
+        Thread.sleep(1*1000);
+        $x("//*[@data-test-id=\"date\"]//self::input").doubleClick().sendKeys(Keys.DELETE, secondMeetingDate);
+        $x("//span[text()='Запланировать']").click();
+        $x("//*[@data-test-id=\"replan-notification\"]").shouldBe(visible).shouldHave(text("Необходимо подтверждение\n" + "У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        Thread.sleep(1*1000);
+        $x("//span[text()='Перепланировать']").click();
+        $x("//*[@data-test-id=\"success-notification\"]").shouldBe(visible).shouldHave(text("Успешно!\n" + "Встреча успешно запланирована на " + secondMeetingDate));
+        Thread.sleep(1*1000);
     }
 }
